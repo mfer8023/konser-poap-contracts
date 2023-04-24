c724c3dcf17f3e4f7ff06d2003fb10de5f4a3923
8023 <mfer8023@gmail.com>
2023-04-24 21:57:09 +0700

    changed pragma from to ^0.8.17

--- contracts/sample/SamplePoapV2.sol
+++ contracts/sample/SamplePoapV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import {ERC2771ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
 
