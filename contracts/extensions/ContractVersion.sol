9989ef5415f367b7715827c7f172db56265955ca
8023 <mfer8023@gmail.com>
2023-05-06 11:22:09 +0700

    edited

--- contracts/extensions/ContractVersion.sol
+++ contracts/extensions/ContractVersion.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /**
  * @title ContractVersion
