983614d5165352f5d76f8eac16d3d26f40cd75ea
8023 <mfer8023@gmail.com>
2023-06-08 21:07:55 +0700

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+const { 
+  API_URL_MATIC,
+  POLYGONSCAN_API_KEY,
+  PRIVATE_KEY_KONSER_DEPLOYER,
+  REPORT_GAS,
+  COINMARKETCAP_API_KEY 
+} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,8 +17,27 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 5000,
+        runs: 2000,
       },
     },
   },
+
+  networks: {
+    matic: {
+      url: API_URL_MATIC,
+      accounts: [`0x${PRIVATE_KEY_KONSER_DEPLOYER}`]
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      polygon: POLYGONSCAN_API_KEY
+    },
+  },
+
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: "ETH",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+  }
 };
